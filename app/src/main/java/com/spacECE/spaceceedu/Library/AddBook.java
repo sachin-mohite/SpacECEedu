2363fde49fbb90e1b533b9755eb5d1ddfc6e58af
lord <ramashisx@gmail.com>
2022-01-02 11:25:00 +0530

    merged changes from avinash

---
+++ app/src/main/java/com/spacECE/spaceceedu/Library/AddBook.java
@@ -0,0 +1,14 @@
+package com.spacECE.spaceceedu.Library;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.spacECE.spaceceedu.R;
+
+public class AddBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book);
+    }
+}
--- app/src/main/java/com/spacECE/spaceceedu/Library/Library_main.java
+++ app/src/main/java/com/spacECE/spaceceedu/Library/Library_main.java
@@ -1,22 +1,38 @@
 package com.spacECE.spaceceedu.Library;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.spacECE.spaceceedu.MainActivity;
 import com.spacECE.spaceceedu.R;
 import com.spacECE.spaceceedu.Utils.UsefulFunctions;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.Inet4Address;
+import java.net.MulticastSocket;
 import java.util.ArrayList;
 
 public class Library_main extends AppCompatActivity {
 
     Button books;
+
+    RecyclerView recyclerView;
+    public FloatingActionButton floatingActionButton;
+
+    ArrayList<String> book_name,book_price,book_category;
+    BottomAppBar bottomAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +44,38 @@ public class Library_main extends AppCompatActivity {
         boolean[] COMPLETED = {false};
         JSONObject[] apiCall = {null};
 
-
-        books = findViewById(R.id.My_books) ;
-
-       books.setOnClickListener(new View.OnClickListener() {
-
+        floatingActionButton=findViewById(R.id.floatingActionBtnBottom);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),My_books.class);
-                startActivity(i);
+            public void onClick(View view) {
+                Intent intent=new Intent(Library_main.this, AddBook.class);
+                startActivity(intent);
             }
-       });
+        });
+
+        bottomAppBar=findViewById(R.id.bottomAppBar);
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                if(item.getItemId()==R.id.menuChat){
+                    Intent i=new Intent(Library_main.this, chatUS.class);
+                    startActivity(i);
+                }
+                if(item.getItemId()==R.id.menuBook){
+                    Intent i=new Intent(Library_main.this, My_books.class);
+                    startActivity(i);
+                }
+                if(item.getItemId()==R.id.menuMaps){
+                    Intent i=new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse("geo:47.4925,19.0513"));
+                    Intent chooser=Intent.createChooser(i,"Lauch Maps");
+                    startActivity(chooser);
+
+                }
+                return false;
+            }
+        });
 
         Thread thread = new Thread(new Runnable() {
 
